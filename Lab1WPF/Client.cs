e402c694178c97525123e239dc60be0ec2c88cd4
stafe_box <stafe_box@outlook.com>
2023-02-23 21:35:43 +0500

    Добавьте файлы проекта.

---
+++ Lab1WPF/Client.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1WPF
+{
+    internal class Client
+    {
+        public delegate void Callback(string responce); 
+        public event Callback? Notify;
+        public void Connect(String server, String message)
+        {
+            string ret = "";
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer
+                // connected to the same address as specified by the server, port
+                // combination.
+                Int32 port = 13000;
+                TcpClient client = new TcpClient(server, port);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer.
+                stream.Write(data, 0, data.Length);
+
+                Debug.Write($"Sent: {message}\n");
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
+                ret = responseData;
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                ret = ($"ArgumentNullException: {e}");
+            }
+            catch (SocketException e)
+            {
+                ret = ($"SocketException: {e}");
+            }
+            Notify?.Invoke(ret);
+        }
+    }
+}
---
+++ Lab1WPF/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab1WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Launch_Click(object sender, RoutedEventArgs e)
+        {
+            string ip = IPBox.Text;
+            string msg = MessageBox.Text;
+            Launch.IsEnabled = false;
+            Client client = new Client();
+            client.Notify += Client_Notyfy;
+            await Task.Run(() => { client.Connect(ip, msg); });
+        }
+
+        private void Client_Notyfy(string responce)
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                Result.Text = responce;
+                Launch.IsEnabled = true;
+            }));
+        }
+    }
+}
